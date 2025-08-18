d4356a161637e22309f1afb669b50bc3faf73b63
unknown <vaibhabtiwari9887@gmail.com>
2025-08-18 14:29:31 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express"); // 1. Import the Express module (which is a function)
+const app = express(); // 2. Execute the function to create your app
+const PORT = 8080;
+app.use(express.json());
+app.listen(PORT, () => console.log("api is on"));
+app.get("/try", (req, res) => {
+  res.status(200).send("active");
+});
+app.post("/data", (req, res) => {
+  res.send(req.body);
+});
