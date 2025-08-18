a4ed86e570552636f2f8ac5c0c0c2ecfc4e6ba34
VanderLord008 <90377070+VanderLord008@users.noreply.github.com>
2025-08-18 14:51:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express"); // 1. Import the Express module (which is a function)
 const app = express(); // 2. Execute the function to create your app
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.listen(PORT, () => console.log("api is on"));
 app.get("/try", (req, res) => {
